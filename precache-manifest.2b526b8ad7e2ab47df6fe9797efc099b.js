886d295d1bfed26ad67484c13cbf8f96ede945dc
Edward <edwardhhf0102@gmail.com>
2020-11-15 21:31:10 +0800

    Updates

---
+++ precache-manifest.2b526b8ad7e2ab47df6fe9797efc099b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8ea4b1139509e00b34065742bddfc8",
+    "url": "/Avatar/index.html"
+  },
+  {
+    "revision": "e706fe965d3a826d98bb",
+    "url": "/Avatar/static/js/2.17e754dc.chunk.js"
+  },
+  {
+    "revision": "a1167c44eacd79c92b31",
+    "url": "/Avatar/static/js/main.af29e5a5.chunk.js"
+  },
+  {
+    "revision": "4a8e4b1ca8c0c03e2ede",
+    "url": "/Avatar/static/js/runtime-main.52252b52.js"
+  },
+  {
+    "revision": "046230a2c479089180d5fd103965b8f0",
+    "url": "/Avatar/static/media/bulbasaur.046230a2.svg"
+  },
+  {
+    "revision": "bdb8ff2f257cc722d230a9311e517631",
+    "url": "/Avatar/static/media/charmander.bdb8ff2f.svg"
+  },
+  {
+    "revision": "3c0ae52bea3f8650a76aa04ff003bc6c",
+    "url": "/Avatar/static/media/eevee.3c0ae52b.svg"
+  },
+  {
+    "revision": "c35557fd5bf600207bffea1e898e957d",
+    "url": "/Avatar/static/media/gengar.c35557fd.svg"
+  },
+  {
+    "revision": "95d6187a02e17da1beae922244f34011",
+    "url": "/Avatar/static/media/jigglypuff.95d6187a.svg"
+  },
+  {
+    "revision": "e78d19dbc7d0f1ead7e8125ab34198c9",
+    "url": "/Avatar/static/media/meowth.e78d19db.svg"
+  },
+  {
+    "revision": "2d5ba1aa7d474f98a38e88041491c4ca",
+    "url": "/Avatar/static/media/pikachu.2d5ba1aa.svg"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/Avatar/static/media/pokeball.2b4798f5.svg"
+  },
+  {
+    "revision": "bf99537ff304b89f0ea245b5059982b8",
+    "url": "/Avatar/static/media/squirtle.bf99537f.svg"
+  }
+]);
